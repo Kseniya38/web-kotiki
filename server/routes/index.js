726726f4af5ba9ae6b9ccc95c3b3fefb19801897
Vityusha <Vrst33@gmail.com>
2024-03-12 10:03:03 +0800

    Роутеры и контроллер справочников животного

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,15 @@ const router = new Router()
 const userRouter = require('./userRouter')
 const noticeRouter = require('./noticeRouter')
 const noticeStatusRouter = require('./noticeStatusRouter')
+const referenceBooksRouter = require('../routes/referenceBooksRouter')
+//const animalRouter = require('./animalRouter')
+//const noticeRouter = require('./noticeRouter')
 
 router.use('/user', userRouter)
 router.use('/notice',noticeRouter)
 router.use('/noticeStatus',noticeStatusRouter)
+//router.use('/notice', noticeRouter)
+//router.use('/animal', animalRouter)
+router.use('/referenceBooks', referenceBooksRouter)
 
 module.exports = router
