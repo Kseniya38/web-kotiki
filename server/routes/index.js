74a755c4f9c835b79d8e422cc2fc5d2c9233529d
santamouse <shpakovakseniya38@gmail.com>
2024-03-12 09:43:17 +0800

    исправлено routers на routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const userRouter = require('./userRouter')
+const noticeRouter = require('./noticeRouter')
+const noticeStatusRouter = require('./noticeStatusRouter')
 
 router.use('/user', userRouter)
+router.use('/notice',noticeRouter)
+router.use('/noticeStatus',noticeStatusRouter)
 
 module.exports = router
