163e9206da243ee7a0bca92707f606752c6316b4
Vityusha <Vrst33@gmail.com>
2024-06-21 18:20:41 +0800

    Отдача фронтенда по любому пути, не входящему в статику и api.

--- server/index.js
+++ server/index.js
@@ -14,10 +14,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/static', express.static(path.join(__dirname, 'static')))
-app.use(express.static(path.join(__dirname, '../client/dist')))
 app.use(fileUpload({}))
 app.use('/api', router)
-
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// Catch-all route to serve the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..' ,'client', 'dist', 'index.html'));
+});
 // Обработка ошибок, последний middleware
 app.use(errorHandler)
 
